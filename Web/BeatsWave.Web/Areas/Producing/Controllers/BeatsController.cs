4befed734add31394596551afc4482be6a4529e7
Georgi Popov <popovgeorgi002@gmail.com>
2020-04-01 23:16:26 +0300

    working on edit beat page

---
+++ Web/BeatsWave.Web/Areas/Producing/Controllers/BeatsController.cs
@@ -0,0 +1,17 @@
+﻿namespace BeatsWave.Web.Areas.Producing.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class BeatsController : Controller
+    {
+        public IActionResult Edit(int beatId)
+        {
+            return this.View();
+        }
+    }
+}
