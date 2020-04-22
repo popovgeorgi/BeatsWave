5239cad6a5c68ba6e88765bc980ea7069929a226
Georgi Popov <popovgeorgi002@gmail.com>
2020-04-22 19:18:40 +0300

    new start page

--- Tests/BeatsWave.Services.Data.Tests/LikeServiceTests.cs
+++ Tests/BeatsWave.Services.Data.Tests/LikeServiceTests.cs
@@ -80,40 +80,5 @@
 
             Assert.False(result);
         }
-
-        [Fact]
-
-        public async Task CheckIfItGetsUsersWhoLikedTheBeat()
-        {
-            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-               .UseInMemoryDatabase("BeatsWaveVirtual");
-            var likeRepository = new EfRepository<Like>(new ApplicationDbContext(options.Options));
-            var beatRepository = new EfDeletableEntityRepository<Beat>(new ApplicationDbContext(options.Options));
-            var userRepository = new EfDeletableEntityRepository<ApplicationUser>(new ApplicationDbContext(options.Options));
-            var service = new LikeService(likeRepository, beatRepository, userRepository);
-
-            var mockedService = new Mock<ILikeService>();
-
-            mockedService.Setup(p => p.GetFansLikedTheBeat(1)).Returns(new List<FanViewModel>
-            {
-                new FanViewModel
-                {
-                    Name = "Pesho",
-                    ImageUrl = "prpr",
-                },
-                new FanViewModel
-                {
-                    Name = "Gosho",
-                    ImageUrl = "prpr",
-                },
-            });
-
-
-            await service.VoteAsync(1, "1");
-            await service.VoteAsync(1, "2");
-            var users = service.GetFansLikedTheBeat(1);
-            
-
-        }
     }
 }
