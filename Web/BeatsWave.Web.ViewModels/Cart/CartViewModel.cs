6e686eecf979c420e2977dab3d60d638842853ab
Georgi Popov <popovgeorgi002@gmail.com>
2020-04-25 13:14:52 +0300

    Shopping Cart is now a ViewComponent

---
+++ Web/BeatsWave.Web.ViewModels/Cart/CartViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeatsWave.Web.ViewModels.Cart
+{
+    public class CartViewModel
+    {
+        public int CartCount { get; set; }
+    }
+}
---
+++ Web/BeatsWave.Web/ViewComponents/CartViewComponent.cs
@@ -0,0 +1,31 @@
+﻿namespace BeatsWave.Web.ViewComponents
+{
+
+    using BeatsWave.Data.Models;
+    using BeatsWave.Services.Data;
+    using BeatsWave.Web.ViewModels.Cart;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ViewComponent(Name = "Cart")]
+    public class CartViewComponent : ViewComponent
+    {
+        private readonly ICartsService cartsService;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public CartViewComponent(ICartsService cartsService, UserManager<ApplicationUser> userManager)
+        {
+            this.cartsService = cartsService;
+            this.userManager = userManager;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var userId = this.userManager.GetUserId(this.Request.HttpContext.User);
+            var viewModel = new CartViewModel();
+            viewModel.CartCount = this.cartsService.Count(userId);
+
+            return this.View(viewModel);
+        }
+    }
+}
